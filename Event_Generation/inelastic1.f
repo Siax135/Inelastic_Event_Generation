2c5cf4c5555eee7cb832f8256375e8272f939970
Keegan Sherman <yahoofunnybones@gmail.com>
2016-07-01 10:57:20 -0400

    Moving files around for a more complete package

---
+++ Event_Generation/inelastic1.f
@@ -0,0 +1,247 @@
+      program inelastic1
+
+
+C...Double precision and integer declarations.
+      IMPLICIT DOUBLE PRECISION(A-H, O-Z)
+      IMPLICIT INTEGER(I-N)
+      INTEGER PYK,PYCHGE,PYCOMP
+C...PYTHIA Commonblocks.
+      COMMON/PYDAT1/MSTU(200),PARU(200),MSTJ(200),PARJ(200)
+      COMMON/PYDAT2/KCHG(500,4),PMAS(500,4),PARF(2000),VCKM(4,4)
+      COMMON/PYDAT3/MDCY(500,3),MDME(8000,2),BRAT(8000),KFDP(8000,5)
+      COMMON/PYDAT4/CHAF(500,2)
+      CHARACTER CHAF*16
+      COMMON/PYDATR/MRPY(6),RRPY(100)
+      COMMON/PYSUBS/MSEL,MSELPD,MSUB(500),KFIN(2,-40:40),CKIN(200)
+      COMMON/PYPARS/MSTP(200),PARP(200),MSTI(200),PARI(200)
+      COMMON/PYINT1/MINT(400),VINT(400)
+      COMMON/PYINT2/ISET(500),KFPR(500,2),COEF(500,20),ICOL(40,4,2)
+      COMMON/PYINT3/XSFX(2,-40:40),ISIG(1000,3),SIGH(1000)
+      COMMON/PYINT4/MWID(500),WIDS(500,5)
+      COMMON/PYINT5/NGENPD,NGEN(0:500,3),XSEC(0:500,3)
+      COMMON/PYINT6/PROC(0:500)
+      CHARACTER PROC*28
+      COMMON/PYINT7/SIGT(0:6,0:6,0:5)
+      COMMON/PYMSSM/IMSS(0:99),RMSS(0:99)
+      COMMON/PYSSMT/ZMIX(4,4),UMIX(2,2),VMIX(2,2),SMZ(4),SMW(2),
+     &SFMIX(16,4),ZMIXI(4,4),UMIXI(2,2),VMIXI(2,2)
+      COMMON/PYMSRV/RVLAM(3,3,3), RVLAMP(3,3,3), RVLAMB(3,3,3)
+      COMMON/PYTCSM/ITCM(0:99),RTCM(0:99)
+      COMMON/PYBINS/IHIST(4),INDX(1000),BIN(20000)
+      COMMON/PYLH3P/MODSEL(200),PARMIN(100),PAREXT(200),RMSOFT(0:100),
+     &     AU(3,3),AD(3,3),AE(3,3)
+      COMMON/PYLH3C/CPRO(2),CVER(2)
+      CHARACTER CPRO*12,CVER*12
+
+c...Required variables
+      integer I,MJ,MPARN,NUMEV,NEULOC,MPILOC,MELECLOC,NGENEV
+      integer NPRINT,NSEED
+      integer MCHARGE(-2212:2212)
+      logical NEU,PION,TEST
+      character ARG*32,OUTPUT*32
+      real ELECANG, THETAMIN, THETAMAX
+      double precision P(4000,5),V(4000,5)
+      integer N,NPAD,K(4000,5)
+      COMMON/PYJETS/N,NPAD,K,P,V
+      COMMON/PIDCHRG/MCHARGE
+
+
+      SAVE /PYDAT1/,/PYDAT2/,/PYDAT3/,/PYDAT4/,/PYDATR/,/PYSUBS/,
+     &/PYPARS/,/PYINT1/,/PYINT2/,/PYINT3/,/PYINT4/,/PYINT5/,
+     &/PYINT6/,/PYINT7/,/PYMSSM/,/PYSSMT/,/PYMSRV/,/PYTCSM/,
+     &/PYBINS/,/PYLH3P/,/PYLH3C/,/PIDCHRG/
+
+      NEU = .FALSE.
+      PION = .FALSE.
+      MPARN = 0
+      NUMEV = 0
+      PARP(2) = 2D0
+c	MSTP(11) = 0
+
+c...Set argument defaults
+      OUTPUT = 'out.dat'
+      NGENEV = 20
+      NPRINT = 5
+      THETAMIN = 0
+      THETAMAX = 90
+	NSEED = 19780503  ! default set by PYTHIA
+	TEST = .FALSE.
+
+c...Set up format for the LUND format
+100   format(11X,I2,     ! Number of particles
+     1 2X,I1,            ! Num of target nucleons
+     2 2X,I1,            ! Num of target protons
+     3 2X,F5.3,          ! Target polarization
+     4 2X,F5.3,          ! Beam polarization
+     5 2X,F5.3,          ! x
+     6 1X,F5.3,          ! y
+     7 2X,F5.3,          ! W
+     8 1X,F5.3,          ! Q^2
+     9 2X,F5.3)          ! nu
+
+200   format(4X,I2,      ! Index
+     1 3X,I2,            ! Charge
+     2 8X,I1,            ! Type
+     3 8X,I7,            ! PID
+     4 7X,I2,            ! Parent Index
+     5 7X,I2,            ! Daughter Index
+     6 8X,F7.4,          ! Px
+     7 3X,F7.4,          ! Py
+     8 3X,F7.4,          ! Pz
+     9 3X,F7.4,          ! E
+     1 3X,F7.4,          ! mass
+     2 3X,F8.4,          ! Vertex x
+     3 3X,F8.4,          ! Vertex y
+     4 3X,F8.4)          ! Vertex z
+
+c...Set format to show given run parameters
+300   format('Run Parameters',/,
+     1 'Output file:',13X,A32,/,
+     2 'Num events:',6X,I10,/,
+     3 'Num events/print:',I10,/,
+     4 'Theta min:',14X,F7.4,/,
+     5 'Theta max:',15X,F7.4,/,
+     6 'RNG seed:',15X,I9)
+
+c...Parse given arguments
+      J = 1
+      do
+        call get_command_argument(J,ARG)
+        if(LEN_TRIM(ARG) .EQ. 0) exit
+
+        if(TRIM(ARG) .EQ. '-o') then
+          J = J+1
+          call get_command_argument(J,OUTPUT)
+        else if(TRIM(ARG) .EQ. '-n') then
+          J = J+1
+          call get_command_argument(J,ARG)
+          read(ARG,'(I10)') NGENEV
+        else if(TRIM(ARG) .EQ. '-n_print') then
+          J = J+1
+          call get_command_argument(J,ARG)
+          read(ARG,'(I10)') NPRINT
+        else if(TRIM(ARG) .EQ. '-theta_min') then
+          J = J+1
+          call get_command_argument(J,ARG)
+          read(ARG,*) THETAMIN
+        else if(TRIM(ARG) .EQ. '-theta_max') then
+          J = J+1
+          call get_command_argument(J,ARG)
+          read(ARG,*) THETAMAX
+	  else if(TRIM(ARG) .EQ. '-seed') then
+	    J = J+1
+	    call get_command_argument(J,ARG)
+	    read(ARG,'(I9)') NSEED
+	  else if(TRIM(ARG) .EQ. '-test') then
+	    J = J+1
+	    call get_command_argument(J,ARG)
+	    read(ARG,'(L3)') TEST
+        else if(TRIM(ARG) .EQ. '-h') then
+          write(*,*) 'Options:'
+          write(*,*) '-o           Output file name (default: out.dat)'
+          write(*,*) '-n           Number of events to generate (default
+     +: 20)'
+          write(*,*) '-n_print     Number of events between print statem
+     +ents (default: 5)'
+          write(*,*) '-theta_min   Minimum electron angle (default: 0)'
+          write(*,*) '-theta_max   Maximum electron angle (default: 90)'
+	    write(*,*) '-seed        Seed for RNG, allowed values 0 <= see
+     +d <= 900000000 (default: 19780503)'
+          goto 40
+        endif
+        J = J+1
+      enddo
+
+c...Check that given theta values aren't backwards
+      if( THETAMAX < THETAMIN ) then
+        write(*,*) 'Theta max is less than theta min!'
+        write(*,*) 'Stopping execution!'
+        goto 40
+      endif
+
+c...Print run parameters
+      write(*,300) OUTPUT,NGENEV,NPRINT,THETAMIN,THETAMAX,NSEED
+
+      open(2,file=OUTPUT)
+
+	MRPY(1) = NSEED
+
+c...Start PYTHIA stuff
+      write(*,*) 'Starting Initialization'
+
+c...Initialize everything, FIXT tells pythia that I have a beam hitting a fixed target
+      call pyinit('FIXT','gamma/e-','p+',11D0)
+
+      write(*,*) 'Initialized'
+
+c...If the test option is set to be true then the values in the MCHARGES array
+c...will be printed to screen for test purposes. This option is not shown
+c...in the documention or in the help message
+	if(TEST) then
+	  call test_charge_array
+	endif
+
+	write(*,*) 'Starting Event Generation'
+
+c...Generate events, start of main loop
+      do I=0,NGENEV
+
+      if( MOD(I,NPRINT) .EQ. 0) then
+        write(*,*) 'Event: ',I
+      endif
+
+      call pyevnt
+
+c...Check to see if we have a neutron, if so then grab its parent
+        do MJ=1,N
+          if (K(MJ,2) .EQ. 2112) then
+            NEULOC = MJ
+            MPARN = K(MJ,3)
+            NEU = .TRUE.
+            goto 20
+          endif
+        enddo
+
+20      if (K(MPARN,2) .NE. 2214) then
+          NEU = .FALSE.
+          NUELOC = -1
+          goto 10
+        endif
+
+        do MJ=1,N
+          if (K(MJ,2) .EQ. 211 .AND. K(MJ,3) .EQ. MPARN) then
+            MPILOC = MJ
+            PION = .TRUE.
+          endif
+        enddo
+
+        do MJ=3,N
+          if (K(MJ,2) .EQ. 11) then
+            MELECLOC = MJ
+            ELECANG = ACOS((P(MELECLOC,3)/P(MELECLOC,4)))*(180/3.14159)
+            goto 30
+          endif
+        enddo
+
+30      if (NEU .AND. PION .AND. (ELECANG < THETAMAX .AND.
+     +    ELECANG > THETAMIN)) then
+          call pyedit(1)
+
+          write(2,100) N,1,1,0.000,0.000,0.000,0.000,0.000,0.000,0.000
+          do MJ=1,N
+            write(2,200) MJ,MCHARGE(K(MJ,2)),1,K(MJ,2),K(MJ,3),0,
+     +          P(MJ,1),P(MJ,2),P(MJ,3),P(MJ,4),
+     +          P(MJ,5),V(MJ,1)/10,V(MJ,2)/10,
+     +          V(MJ,3)/10
+          enddo
+
+          NUMEV = NUMEV+1
+        endif
+
+10    enddo
+c...End of main loop
+
+      print*,"Total events in ",TRIM(OUTPUT),": ",NUMEV
+
+40    stop
+      end
---
+++ Neutron_Efficiency_Analysis/neutronEfficiency.groovy
@@ -0,0 +1,258 @@
+import org.jlab.evio.clas12.*;
+import org.jlab.clas.physics.*;
+import org.jlab.clas12.physics.*;
+import org.root.histogram.*;
+import org.root.pad.*;
+import org.root.group.*;
+import org.root.func.*;
+import java.lang.Math;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.lang.String;
+import java.lang.Integer;
+import java.lang.Double;
+import org.jlab.geom.*;
+import org.jlab.geom.detector.ec.*;
+import org.jlab.geom.base.*;
+import org.jlab.geom.detector.ec.*;
+import org.jlab.clas12.dbdata.DataBaseLoader.*;
+import org.jlab.clas12.dbdata.*;
+import org.jlab.clasrec.utils.*
+import org.jlab.geom.prim.*;
+import org.jlab.geom.detector.ec.ECFactory.*;
+
+// open file
+EvioSource reader = new EvioSource();
+reader.open("/home/Siax/shared/nopcalData/neutrons_12-15_NOPCAL_Rec.0.evio");
+
+// create fitter to get data
+GenericKinematicFitter fitter = new GenericKinematicFitter(11.0);
+
+//* Create variables for later use **************************************************
+// variables for reading in data
+EvioDataBank genBank;
+EvioDataBank ecBank;
+EvioDataEvent event;
+PhysicsEvent genEvent;
+PhysicsEvent recEvent;
+Particle genElectron;
+Particle recNeutron;
+
+// various counters, indexes, and general information variables
+int nevents, nentries, nrec = 0;
+int genRows, ecRows, electronIndex = 0;
+int genNeutronCount, recNeutronCount, genElectronCount, recElectronCount,neutronCount, electronCount = 0;
+double electronPx, electronPy, electronPz = 0;
+double hitX, hitY, hitZ = 0;
+double neutronPmag, hitMag = 0;
+double thetapq, momentum = 0;
+double nde, dnde = 0;
+
+// variables for keeping track of relevant particles paths and hits
+Vector3D scatteredElectron;
+Vector3D scatteredNeutron;
+Vector3D generatedNeutron;
+Vector3D ECHit;
+
+// variables for setting up and using the CLAS12 geometry
+Line3D neutronPath;
+ArrayList sectors, superlayes, layers;
+ECSector sector;
+ECSuperlayer superlayer;
+ECLayer layer;
+Shape3D[] ECfaces = new Plane3D[6];
+boolean intersectionStatus = false;
+
+ // keep track of skipped events
+int skippedEvents = 0;
+
+// used to look for reconstructed electron info
+Scanner search;
+String recLund, electronInfo;
+
+// used to tell if we found a reconstructed electron
+boolean electronFound = false
+
+// constants
+final int BIN_NUM = 50;
+final double BEAM_ENERGY = 11.0;
+//**********************************************************************************
+
+//* Geometry set-up ****************************************************************
+ConstantProvider constants = DataBaseLoader.getConstantsEC();
+ECDetector detector = new ECFactory().createDetectorCLAS(constants);
+
+sectors = detector.getAllSectors();
+
+// loop over each sector and move down the hierarchy to get a plane in place of the front face of each sector of the EC
+for(int i = 0; i < 6; i++){
+    sector = sectors.get(i);
+    superlayers = sector.getAllSuperlayers();
+    superlayer = superlayers.get(0);
+    layers = superlayer.getAllLayers();
+    layer = layers.get(0);
+    ECfaces[i] = layer.getBoundary();
+} // end loop over sectors
+//**********************************************************************************
+
+//* Create histograms **************************************************************
+TDirectory histFile = new TDirectory();
+histFile.mkdir("neutrons");
+
+histFile.getDirectory("neutrons").add(new H1D("hthetapq", 100, 0, 10));
+H1D hthetapq = (H1D)histFile.getDirectory("neutrons").getObject("hthetapq");
+hthetapq.setXTitle("thetapq with cut (deg)");
+
+histFile.getDirectory("neutrons").add(new H1D("htheta", 100, 0, 10));
+H1D htheta = (H1D)histFile.getDirectory("neutrons").getObject("htheta");
+htheta.setXTitle("thetapq (deg)");
+
+histFile.getDirectory("neutrons").add(new H1D("hmomentumTotal", BIN_NUM, 0, BEAM_ENERGY));
+H1D hmomentumTotal = (H1D)histFile.getDirectory("neutrons").getObject("hmomentumTotal");
+hmomentumTotal.setXTitle("momentum (GeV/c)");
+
+histFile.getDirectory("neutrons").add(new H1D("hmomentumRec", BIN_NUM, 0, BEAM_ENERGY));
+H1D hmomentumRec = (H1D)histFile.getDirectory("neutrons").getObject("hmomentumRec");
+hmomentumRec.setXTitle("momentum (GeV/c)");
+
+histFile.getDirectory("neutrons").add(new H2D("hNDE", BIN_NUM, 0, BEAM_ENERGY, 100, 0, 1));
+H2D hNDE = (H2D)histFile.getDirectory("neutrons").getObject("hNDE");
+hNDE.setXTitle("momentum (GeV/c)");
+hNDE.setYTitle("NDE");
+//**********************************************************************************
+
+//loop over events
+while(reader.hasEvent()){
+    // get generated and reconstructed events
+    event = reader.getNextEvent();
+    genEvent = fitter.getGeneratedEvent(event);
+    recEvent = fitter.getPhysicsEvent(event);
+
+    // get generated and reconstructed electron counts
+    genElectronCount = genEvent.countByPid(11);
+    recElectronCount = recEvent.countByPid(11);
+    electronCount = Math.max(genElectronCount, recElectronCount);
+
+    // get generated and reconstructed electron counts
+    genNeutronCount = genEvent.countByPid(2112);
+    recNeutronCount = recEvent.countByPid(2112);
+    if(recNeutronCount > 0) System.out.println(nevents + ": " + recNeutronCount);
+    neutronCount = Math.max(genNeutronCount, recNeutronCount);
+
+    // make sure we have electrons and neutrons to analyze
+    if(electronCount > 0 && neutronCount > 0){
+        // get information about the reconstruction in lund format and pass it to a scanner
+        electronFound = false;
+        recLund = recEvent.toLundString();
+        search = new Scanner( recLund );
+        numRecParticles = Integer.parseInt( search.next() );
+        search.nextLine();
+
+        // loop over each reconstructed particle looking for an electron
+        for( int i = 0; i < numRecParticles; i++ ) {
+            for( int j = 0; j < 4; j++ ) { // jump to pid in lund string
+                electronInfo = search.next();
+            } // end loop jumping to pid
+            recPID = Integer.parseInt(electronInfo);
+            if( recPID == 11 ) { // check if current pid means we have an electron
+                for( int k = 0; k < 3; k++ ) { // jump to px for electron
+                    electronInfo = search.next();
+                } // end loop to jump to px
+                // get px, py, and pz from lund string
+                electronPx = Double.parseDouble(electronInfo);
+                electronInfo = search.next();
+                electronPy = Double.parseDouble(electronInfo);
+                electronInfo = search.next();
+                electronPz = Double.parseDouble(electronInfo);
+                electronFound = true; // we found an electron
+                break;
+            } // end check of the pid
+
+            search.nextLine(); // jump to next line if we didn't find an electron
+        } // end loop through reconstructed particles
+
+        if(!electronFound){ // make sure we actually found a reconstructed electron
+            skippedEvents++;
+            //nevents++;
+            continue;
+        } // end if for checking that we have a reconstructed electron
+
+        if(event.hasBank("ECRec::clusters")){ // make sure we have the ECRec::clusters bank
+            ecBank = event.getBank("ECRec::clusters");
+            ecRows = ecBank.rows();
+        }else{ // if we don't have the bank then move on to the next event
+            skippedEvents++;
+            // nevents++;
+            continue;
+        } // end if for the ECRec::clusters bank
+
+        // calculate scattered neutron momentum
+        scatteredNeutron = new Vector3D(-1*electronPx, -1*electronPy, (BEAM_ENERGY-electronPz));
+        momentum = scatteredNeutron.mag(); // store magnitude in GeV
+        scatteredNeutron.scale(1000); // scale scatter neutron vector to MeV
+        scatteredNeutron.unit(); // make scattered neutron momentum vector a unit vector
+
+        genBank = event.getBank("GenPart::true");
+        generatedNeutron = new Vector3D(genBank.getDouble("px",1), genBank.getDouble("py",1), genBank.getDouble("pz",1))
+        generatedNeutron.unit();
+
+
+        // create a line representing the neutron's path and loop over EC sectors to see if the neutron would hit the sectors
+        neutronPath = new Line3D( new Point3D(0,0,0), generatedNeutron );
+        for(int i = 0; i < 6 && !intersectionStatus; i++){
+            intersectionStatus = ECfaces[i].hasIntersection(neutronPath);
+        } // end loop over sectors to see if neutrons hit
+
+        if(!intersectionStatus){ // if neutron wouldn't hit then skip event
+            skippedEvents++;
+            continue;
+        } // end if checking that the neutron doesn't miss
+
+        hmomentumTotal.fill(momentum);
+
+        // loop over neutron candidates
+        for(int i = 0; i < ecRows; i++){
+            // get hit coordinates and make a vector
+            hitX = ecBank.getDouble("X",i);
+            hitY = ecBank.getDouble("Y",i);
+            hitZ = ecBank.getDouble("Z",i);
+            ECHit = new Vector3D(hitX, hitY, hitZ);
+
+            ECHit.unit(); // make hit vector a unit vector
+            //ECHit.unit();
+            thetapq = Math.acos(generatedNeutron.dot(ECHit))*(180/Math.PI); // calculate angle between scattered neutron and neutron hit candidate
+            htheta.fill(thetapq);
+            if(thetapq < 1.5) { // if angle is less than 1.5 degrees then call it a hit and move on to the next event
+                hthetapq.fill(thetapq);
+                hmomentumRec.fill(momentum);
+                break;
+            } // end if checking thetapq
+        } // end loop over neutron candidates
+        nevents++;
+        intersectionStatus = false;
+    } // end if checking electron and neutron count
+} // end loop over events
+
+// calculate neutron detection efficiency and print run info to screen
+System.out.println("Num events: " + nevents + " Skipped events: " + skippedEvents);
+nentries = hmomentumRec.getEntries();
+nde = (double) nentries/nevents;
+dnde = nde*Math.sqrt(nde*(1-nde)/nentries);
+System.out.println("nde= " + nde + " +/- " + dnde);
+
+// get data from momentum histograms
+double[] hmomentumTotalData = hmomentumTotal.getData();
+double[] hmomentumRecData = hmomentumRec.getData();
+
+// calculate steps between bins
+double step = BEAM_ENERGY/BIN_NUM;
+double currentP = step/2;
+
+// loop over data from histograms and create NDE histogram
+for(int i = 0; i < BIN_NUM; i++){
+    hNDE.fill(currentP, hmomentumRecData[i]/hmomentumTotalData[i], hmomentumRecData[i]);
+    currentP += step;
+} // end loop over data
+
+// write histograms to file
+histFile.write("neutrons_12-15_Hist_NOPCAL.evio");
