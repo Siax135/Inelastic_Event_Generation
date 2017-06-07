50904019b3d953d5fa7bb35e5e13ad3f7b87090d
Keegan Sherman <yahoofunnybones@gmail.com>
2017-06-07 11:42:04 -0400

    Removed unneeded imports and variables from neutronEfficiencyTBT.groovy

--- Neutron_Efficiency_Analysis/neutronEfficiencyTBT.groovy
+++ Neutron_Efficiency_Analysis/neutronEfficiencyTBT.groovy
@@ -11,11 +11,9 @@ import org.jlab.detector.base.*;
 import org.jlab.io.hipo.*;
 
 String inputFile = args[0];
-//String caseNum = args[1];
 
 // open file
 HipoDataSource reader = new HipoDataSource();
-//reader.open("/home/Siax/Linux_Shared/Pythia/e_pi_n3_Rec_NEW.0.evio");
 reader.open(inputFile);
 
 //* Create variables for later use **************************************************
@@ -26,15 +24,14 @@ HipoDataBank TOFBank;
 HipoDataEvent event;
 
 // various counters, indexes, and general information variables
-int nevents, nentries, nentriesTotal, nrec, nless = 0;
-int genRows, ecRows, TOFrows, electronIndex, piPlusIndex = 0
+int nevents, nentries, nentriesTotal = 0;
+int ecRows, TOFrows = 0
 int chargeOne, chargeTwo = 0;
 float electronPx, electronPy, electronPz, piPlusPx, piPlusPy, piPlusPz = 0;
 float electronE, piPlusE = 0;
 float energyTerm, momentumTerm = 0;
-float scatteredElectronMassSq, scatteredPiPlusMassSq, missingMass = 0;
+float missingMass = 0;
 float hitX, hitY, hitZ = 0;
-float neutronPmag, hitMag = 0;
 float thetapq, theta, momentum = 0;
 float nde, dnde = 0;
 
@@ -42,7 +39,6 @@ float nde, dnde = 0;
 Vector3D scatteredElectron;
 Vector3D scatteredPiPlus;
 Vector3D scatteredNeutron;
-Vector3D generatedNeutron;
 Vector3D momentumTermVector;
 Vector3D ECHit;
 
@@ -57,13 +53,6 @@ boolean intersectionStatus = false;
 
  // keep track of skipped events
 int skippedEvents = 0;
-
-// used to look for reconstructed electron and pi+ info
-Scanner electronSearch, piPlusSearch;
-String recLund, electronInfo, piPlusInfo;
-
-// used to tell if we found a reconstructed electron
-boolean electronFound = false
 
 // constants
 final int BIN_NUM = 50;
@@ -135,35 +124,31 @@ hNDE.setTitleY("NDE");
 //loop over events
 while(reader.hasEvent()){
     // get generated and reconstructed events
-    //if(nevents > 60) break;
 
-    System.out.println("Grabbed event: " + nevents);
     event = reader.getNextEvent();
 
     TOFrows = 0;
-    //System.out.println("Before if, event: " + nevents + " reset rows: " + TOFrows);
-    if(event.hasBank("TimeBasedTrkg::TBTracks")){
+    if(event.hasBank("TimeBasedTrkg::TBTracks")){ // check to see if the event has the time based tracking bank I need
         TOFBank = event.getBank("TimeBasedTrkg::TBTracks");
         TOFrows = TOFBank.rows();
-        System.out.println("Event: " + nevents + " rows: " + TOFBank.rows() + " what I think: " + TOFrows);
-        if(TOFrows != 2) {
-            System.out.println("Has TimeBasedTrkg bank but not exactly 2 rows");
-            System.out.println();
+        // Since I am only interested in the 3 particle event e pi+ n and n won't be reconstructed 
+        // I only want to look at events that have exactly 2 reconstructed particles ( e and pi+ )
+        if(TOFrows != 2) { 
+            skippedEvents++;
             nevents++;
             continue;
-        }
+        } // end of if to check TOFrows
     } else {
-        System.out.println("No TimeBasedTrkg bank.");
-        System.out.println();
+        skippedEvents++;
         nevents++;
         continue;
-    }
+    } // end of if to check for time based tracking bank.
 
     chargeOne = TOFBank.getInt("q",0);
     chargeTwo = TOFBank.getInt("q",1);
 
-    if(chargeOne == 1 && chargeTwo == -1){
-        System.out.println("Event: " + nevents + ", Proton, Electron");
+    // check which charge is the electron and which is the pi+
+    if(chargeOne == 1 && chargeTwo == -1){ // chargeOne is the pi+ and chargeTwo is the electron
         electronPx = TOFBank.getFloat("p0_x",1);
         electronPy = TOFBank.getFloat("p0_y",1);
         electronPz = TOFBank.getFloat("p0_z",1);
@@ -171,8 +156,7 @@ while(reader.hasEvent()){
         piPlusPx = TOFBank.getFloat("p0_x",0);
         piPlusPy = TOFBank.getFloat("p0_y",0);
         piPlusPz = TOFBank.getFloat("p0_z",0);
-    } else if(chargeOne == -1 && chargeTwo == 1){
-        System.out.println("Event: " + nevents + ", Electron, Proton");
+    } else if(chargeOne == -1 && chargeTwo == 1){ // chargeOne is the electron and chargeTwo is the pi+ 
         electronPx = TOFBank.getFloat("p0_x",0);
         electronPy = TOFBank.getFloat("p0_y",0);
         electronPz = TOFBank.getFloat("p0_z",0);
@@ -180,31 +164,35 @@ while(reader.hasEvent()){
         piPlusPx = TOFBank.getFloat("p0_x",1);
         piPlusPy = TOFBank.getFloat("p0_y",1);
         piPlusPz = TOFBank.getFloat("p0_z",1);
-    } else{
-        System.out.println("Event: " + nevents + ", Similar charges");
-        System.out.println();
+    } else{ // either both charges are the same or one (or both) are 0
+        skippedEvents++;
         nevents++;
         continue;
-    }
+    } // end if to check which charge is which
 
+    // 3-momentum of scattered charged particles
     scatteredElectron = new Vector3D(electronPx, electronPy, electronPz);
     scatteredPiPlus = new Vector3D(piPlusPx, piPlusPy, piPlusPz);
 
+    // Energy of scattered charged particles
     electronE = Math.sqrt(scatteredElectron.dot(scatteredElectron) + Math.pow(ELECTRON_MASS,2));
     piPlusE = Math.sqrt(scatteredPiPlus.dot(scatteredPiPlus) + Math.pow(PI_PLUS_MASS,2));
 
+    // missing mass squared (m^2): m^2 = (E_in - E_out)^2 - (P_in - P_out)^2
+
+    // energy term of the equation to solve for m^2
     energyTerm = Math.pow(ELECTRON_INITIAL_ENERGY + PROTON_INITIAL_ENERGY - electronE - piPlusE, 2);
 
+    // momentum term of the equation to solve for m^2
     momentumTermVector = new Vector3D(0,0,11).sub(scatteredElectron).sub(scatteredPiPlus);
     momentumTerm = momentumTermVector.dot(momentumTermVector);
 
-    // calculate missing mass
+    // calculate m^2
     missingMass = energyTerm - momentumTerm;
-
-    System.out.println("Event: " + nevents + " Calculated missing mass to be: " + missingMass);
 
     hmissingMassHerm.fill(missingMass);
 
+    // If m^2 isn't around what it should be for a neutron then skip the event
     if( missingMass > 0.87 && missingMass < 0.89 ) {
         hmissingMass.fill(missingMass);
 
@@ -212,23 +200,24 @@ while(reader.hasEvent()){
             ecBank = event.getBank("ECAL::clusters");
             ecRows = ecBank.rows();
         }else{ // if we don't have the bank then move on to the next event
+            skippedEvents++;
             nevents++;
             continue;
         } // end if for the ECRec::clusters bank
 
+        // calculate scattered neutron 3-momentum
         scatteredNeutron = new Vector3D(-1*(electronPx+piPlusPx), -1*(electronPy+piPlusPy), (BEAM_ENERGY-electronPz-piPlusPz));
         momentum = scatteredNeutron.mag(); // store magnitude in GeV
         scatteredNeutron.scale(1000); // scale scatter neutron vector to MeV
 
-
-
         neutronPath = new Line3D( new Point3D(0,0,0), scatteredNeutron );
-        for(int j = 0; j < 6 && !intersectionStatus; j++){
+        for(int j = 0; j < 6 && !intersectionStatus; j++){ // loop over EC sectors to see if neutron hits
             intersectionStatus = ECfaces[j].hasIntersection(neutronPath);
         } // end loop over sectors to see if neutrons hit
 
 
         if(!intersectionStatus){ // if neutron wouldn't hit then skip event
+            skippedEvents++;
             nevents++;
             continue;
         } // end if checking that the neutron doesn't miss
@@ -257,8 +246,6 @@ while(reader.hasEvent()){
                 theta = Math.acos(scatteredNeutron.z()/scatteredNeutron.mag())*(180/Math.PI);
                 hacceptance.fill(theta,momentum);
 
-                System.out.println("Event: " + nevents + " P: " + momentum + " Theta: " + theta);
-
                 break;
             } // end if checking thetapq
 
@@ -266,81 +253,14 @@ while(reader.hasEvent()){
         }
 
         intersectionStatus = false;
+    } else {
+        skippedEvents++;
     }
 
-//        if(missingMass < 0.80){
-//            nless++;
-//            System.out.println("Event: " + nevents);
-//            System.out.println("row 1 q: " + TOFBank.getInt("q",0) + " row 2 q: " + TOFBank.getInt("q",1))
-//            System.out.println("Electron: " + scatteredElectron.toString() + " E: " + electronE);
-//            System.out.println("Pi+: " + scatteredPiPlus.toString() + " E: " + piPlusE);
-//            System.out.println("MM: " + missingMass);
-//        }
-
-    //System.out.println("Event: " + nevents);
-    //System.out.println("row 1 q: " + TOFBank.getInt("q",0) + " row 2 q: " + TOFBank.getInt("q",1))
-    //System.out.println("Electron: " + scatteredElectron.toString() + " E: " + electronE);
-    //System.out.println("Pi+: " + scatteredPiPlus.toString() + " E: " + piPlusE);
-    //System.out.println("MM: " + missingMass);
-
-nevents++;
-System.out.println();
+    nevents++;
 }
-
-System.out.println("Events less than 0.88:" + nless);
-
-//TCanvas c1 = new TCanvas("c1","PhysicsAnalysis",1200,640,1,3);
-//c1.cd(0);
-//c1.draw(hmissingMassHerm);
-//c1.cd(1);
-//c1.draw(hmissingMass);
-//c1.cd(2);
-//c1.draw(hacceptance);
-
- 
- 
- 
- 
- 
- 
- 
- 
- 
-
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
-
  
  // calculate neutron detection efficiency and print run info to screen
-//System.out.println("Case: " + caseNum);
 System.out.println("Num events: " + nevents + " Skipped events: " + skippedEvents);
 nentries = hmomentumRec.getEntries();
 nentriesTotal = hmomentumTotal.getEntries();
